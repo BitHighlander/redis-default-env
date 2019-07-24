cdfe2f1fd29b1984e3cb7dd146e637941f780292
highlander <bithighlander@gmail.com>
2019-07-24 16:47:42 -0600

    lets try ioredis

--- index.js
+++ index.js
@@ -19,26 +19,58 @@
 /**
  * Created by highlander on 3/6/17.
  */
-
 let TAG = " | REDIS-CONNECTION-MODULE | "
+let log = require("loggerdog-client")()
+let Redis = require("ioredis");
 
-let config = {}
-config.REDIS_CONNECTION = process.env.REDIS_CONNECTION || "redis://127.0.0.1:6379"
-
-let subscriber
-let publisher
 let redis
-try{
-	const pubsub = require('redis')
-	publisher = pubsub.createClient(config.REDIS_CONNECTION)
-	subscriber = pubsub.createClient(config.REDIS_CONNECTION)
+let publisher
+let subscriber
 
-	const Redis = require('then-redis/lib')
-	redis = Redis.createClient(config.REDIS_CONNECTION)
-}catch(e){
-	console.error(TAG+"e: ",e)
-	//if prod, throw
-	if(process.env("NODE_ENV") === 'production') throw Error("101: Redis misconfiguration! ")
+let redis0
+let redis1
+let redis2
+
+//ask master
+let getMaster = async function(){
+	let tag = TAG + " | getMaster | "
+	try{
+		let isMaster0 = await redis0.role()
+		if(isMaster0[0] === 'master') redis = redis0
+		let isMaster1 = await redis1.role()
+		if(isMaster0[0] === 'master') redis = redis1
+		let isMaster2 = await redis2.role()
+		if(isMaster0[0] === 'master') redis = redis2
+
+		log.info("isMaster0: ",isMaster0)
+		log.info("isMaster1: ",isMaster1)
+		log.info("isMaster2: ",isMaster2)
+
+	}catch(e){
+		log.error(tag,e)
+	}
 }
+
+
+if(process.env.NODE_ENV === 'production'){
+	redis0 = new Redis(process.env.REDIS_CONNECTION_CLUSTER_0)
+	redis1 = new Redis(process.env.REDIS_CONNECTION_CLUSTER_1)
+	redis2 = new Redis(process.env.REDIS_CONNECTION_CLUSTER_2)
+
+	redis = redis0
+	//pub/sub can be anything
+	publisher = redis0
+	subscriber = redis0
+
+
+	getMaster()
+} else {
+	redis = new Redis(process.env.REDIS_CONNECTION)
+	publisher = new Redis(process.env.REDIS_CONNECTION)
+	subscriber = new Redis(process.env.REDIS_CONNECTION)
+}
+
+
+
 
 module.exports = {redis, publisher, subscriber}
--- test/test-module.js
+++ test/test-module.js
@@ -1,5 +1,20 @@
 let {redis,publish,subscribe} = require("../index.js")
 
+//console.log(redis)
+// redis.set("foo","bar",function(error,resp){
+// 	console.log(error,resp)
+// })
+
+//console.log(redis)
+
+// let run = async function(){
+// 	console.log("run")
+// 	let result = await redis.set("test","foo")
+// 	console.log("result: ",result)
+// 	let result1 = await redis.get("test","foo")
+// 	console.log("result1: ",result1)
+// }
+// run()
 
 /*
 write
